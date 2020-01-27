90a5304c0eb18e6c6847a1e5ad2f098604d6e6e3
ipmanlk <io@navinda.xyz>
2020-01-27 10:45:07 +0530

    change: api v1.0 actions

--- api/v1.0/handle.js
+++ api/v1.0/handle.js
@@ -4,60 +4,54 @@ const handle = (req, res) => {
     const params = req.query;
 
     // news post
-    if (params.post_id) {
+    if (params.action == "news-post" && params.post_id) {
         getNews.getNewsPost(params.post_id).then(newsPost => {
             res.json(newsPost);
         }).catch(e => {
             console.log(e);
         });
-        return;
-    }
-
-    // if sources aren't defined
-    if (!params.sources || params.sources.trim() == "") {
-        res.json("Sorry can't find that!");
-        return;
     }
 
     // news sources list
-    if (params.sources == "list" && params.lang) {
+    if (params.action == "news-sources" && params.lang) {
         getNews.getNewsSources(params.lang).then(newsSources => {
             res.json(newsSources);
         }).catch(e => {
             console.log(e);
         });
-        return;
     }
 
     // load more
-    if (params.last_news_id) {
+    if (params.action == "news-list-old" && params.news_id) {
         const sources = params.sources.split(",") || [];
         getNews.getNewsList(params.lang, sources, params.last_news_id).then(newsList => {
             res.json(newsList);
         }).catch(e => {
             console.log(e);
         });
-        return;
     }
 
     // check for new posts
-    if (params.check_news && params.sources) {
+    if (params.action == "news-check" && params.sources) {
         const sources = params.sources.split(",") || [];
         getNews.getLatestId(sources).then(latestId => {
             res.json(latestId);
         }).catch(e => {
             console.log(e);
         });
-        return;
     }
 
+
     // initial news list
-    const sources = params.sources.split(",") || [];
-    getNews.getNewsList(sources).then(newsList => {
-        res.json(newsList);
-    }).catch(e => {
-        console.log(e);
-    });
+    if (params.action == "news-list" && params.sources) {
+        // initial news list
+        const sources = params.sources.split(",") || [];
+        getNews.getNewsList(sources).then(newsList => {
+            res.json(newsList);
+        }).catch(e => {
+            console.log(e);
+        });
+    }
 }
 
 module.exports = {
