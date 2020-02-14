4d1cc8d6b585bbde2627110121b05f3e45e94805
ipmanlk <io@navinda.xyz>
2020-02-14 10:50:34 +0530

    add: check news count on db and auto clear

--- scrapper/storeNews.js
+++ scrapper/storeNews.js
@@ -3,12 +3,17 @@ const cheerio = require('cheerio');
 const moment = require('moment-timezone');
 const db = new SQLite(`${__dirname}/../db/cn.db`);
 
-// save news in database
 const save = async (newsData) => {
     let news = clean(newsData);
     if (!news.title || news.title == "" || news.title.trim() == "") return;
     const currentDateTime = moment().tz('Asia/Colombo').format('YYYY-MM-DD hh:mm A');
-    await db.run("INSERT INTO news(title, post, link, source_id, time, main_img) VALUES(?,?,?,?,?,?)", [news.title, news.post, news.link, news.source_id, currentDateTime, news.main_img]).catch(e => console.log(e));
+
+    // check if news item is already in db
+    const row = await db.getOne("SELECT COUNT(id) AS count FROM news WHERE title = ? OR link = ?", [news.title, news.link]);
+    if (row.count !== 0) return;
+
+    // else save it 
+    await db.run("INSERT INTO news(title, post, link, source_id, time, main_img) VALUES(?,?,?,?,?,?)", [news.title, news.post, news.link, news.source_id, currentDateTime, news.main_img]);
 }
 
 // remove useless elements from news item
@@ -41,6 +46,15 @@ const escapedHtmlFix = (text) => {
         .replace("&amp;#039;", "'");
 };
 
+const checkNewsCount = async () => {
+    const row = await db.getOne("SELECT COUNT(id) as count FROM news");
+    // empty news table if there are too many news
+    if (row.count > 10000) {
+        await db.run("DELETE FROM news");
+    }
+}
+
 module.exports = {
-    save
+    save,
+    checkNewsCount
 }
