d84ac7fb9879d6c719ae95ba9b4e2935ad2b5abc
ipmanlk <io@navinda.xyz>
2020-04-03 22:26:11 +0530

    add: tests for rss feeds

---
+++ test/sources.test.js
@@ -0,0 +1,15 @@
+const Parser = require("rss-parser");
+const parser = new Parser();
+const sources = require("../sources/sources.json");
+
+// settings for jest
+jest.setTimeout(60000);
+
+// Test all RSS feeds
+for (let source of sources) {
+    test(`RSS Feed: ${source.name} (${source.lang})`, async() => {
+        console.log(`Testing ${source.name} (${source.lang})`);
+        const feed = await parser.parseURL(source.feed);
+        expect(feed.items.length).not.toBe(0);
+    });
+}
