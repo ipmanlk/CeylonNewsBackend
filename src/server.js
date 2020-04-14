d06092fe3806f124e63921848c901a47aab84d52
ipmanlk <io@navinda.xyz>
2020-04-15 01:26:14 +0530

    fix: internal server error when path not found

--- src/server.js
+++ src/server.js
@@ -13,13 +13,12 @@ app.get('/:version', (req, res) => {
         const api = require(`./api/${req.params.version}/handle.js`);
         api.handle(req, res);
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ "error": "Internal Server Error." });
+        res.status(404).json({ "error": "Sorry!. I can't find that." });
     }
 });
 
 app.use((req, res, next) => {
-    res.status(404).json({ "error": "Sorry can't find that!" });
+    res.status(404).json({ "error": "Sorry!. I can't find that!" });
 });
 
 app.listen(port, () => {
