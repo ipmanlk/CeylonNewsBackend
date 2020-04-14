993ecd6687110a5db504f278eb864acb69565c6e
ipmanlk <io@navinda.xyz>
2020-04-15 01:46:25 +0530

    change: updated docker run script to mount auth dir

--- dockerrun.sh
+++ dockerrun.sh
@@ -2,4 +2,11 @@
 docker build --tag ceylon_news .
 docker stop cn
 docker rm cn
-docker run -d -p 3000:3000 --name cn ceylon_news 
+# docker run -d -p 3000:3000 --name cn ceylon_news 
+
+docker run -d \
+  -it \
+  -p 3000:3000 \
+  --name cn \
+  --mount type=bind,source="$(pwd)"/auth,target=/usr/src/app/src/api/auth \
+  ceylon_news
