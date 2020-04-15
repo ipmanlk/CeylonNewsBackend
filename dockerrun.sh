31300f9fd9b20c863d9ed21714f0710ed21fda0e
ipmanlk <io@navinda.xyz>
2020-04-15 21:59:36 +0530

    fix: .env mounting to wrong dir

--- dockerrun.sh
+++ dockerrun.sh
@@ -8,6 +8,6 @@ docker run -d \
   -it \
   -p 3000:3000 \
   --name ceylon_news \
-  --mount type=bind,source="$(pwd)"/.env,target=/usr/src/app/src/.env \
+  --mount type=bind,source="$(pwd)"/.env,target=/usr/src/app/.env \
   --mount type=bind,source="$(pwd)"/auth,target=/usr/src/app/src/api/auth \
   ceylon_news
