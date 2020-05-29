f43e65a8c958a6178c47b35014fe31d13ae5348a
ipmanlk <io@navinda.xyz>
2020-05-29 12:08:04 +0530

    Revert "add: sematext metrics to api v2.0"
    This reverts commit 377b9688d5655b17cc49ab655a69753824f0cff0.

--- src/api/v2.0/handle.js
+++ src/api/v2.0/handle.js
@@ -1,14 +1,11 @@
 const tokens = require("../auth/v2.0/tokens.json");
 const newsData = require("./data/newsData");
-const { stLogger } = require("sematext-agent-express");
 
 const handle = (req, res) => {
-
     // check authentication
     const token = req.headers.token;
     if (tokens.indexOf(token) == -1) {
-        stLogger.error("fail: authentication");
-        res.status(401).json({"error": "You are not authorized."});
+        res.status(401).json({"error": "You are not authorized."})
         return;
     }
 
@@ -20,11 +17,9 @@ const handle = (req, res) => {
         const keyword = req.query.keyword || "";
         const skip = req.query.skip || 0;
         newsData.getNewsList(sources, keyword, skip).then(data => {
-            stLogger.info("sent: news-list")
             res.json(data);
         }).catch(e => {
             console.log(e);
-            stLogger.error("fail: news-list server error");
             res.status(500).json({ "error": "Internal Server Error." });
         });
         return;
@@ -34,15 +29,12 @@ const handle = (req, res) => {
     if (action == "news-post") {
         newsData.getNewsPost(req.query.news_id).then(newsPost => {
             if (newsPost == undefined) {
-                stLogger.error("fail: news-post not found with given id")
                 res.status(404).json({ "error": "Unable to locate that post." });
             } else {
-                stLogger.info("sent: news post")
                 res.json(newsPost);
             }
         }).catch(e => {
             console.log(e);
-            stLogger.error("fail: news-post server error");
             res.status(500).json({ "error": "Internal Server Error." });
         });
         return;
@@ -51,11 +43,9 @@ const handle = (req, res) => {
     // retrieve news sources list
     if (action == "news-sources" && req.query.lang) {
         newsData.getNewsSources(req.query.lang).then(newsSources => {
-            stLogger.info("sent: news-sources");
             res.json(newsSources);
         }).catch(e => {
             console.log(e);
-            stLogger.error("fail: news-sources server error")
             res.status(500).json({ "error": "Internal Server Error." });
         });
         return;
