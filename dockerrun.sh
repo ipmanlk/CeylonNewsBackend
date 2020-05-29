acbace3423073526884fe8672d31be5c3cfec319
ipmanlk <io@navinda.xyz>
2020-05-29 12:24:05 +0530

    change: update dockerrun script

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker build --tag ceylon_news .
-docker stop cn
-docker rm cn
-# docker run -d -p 3000:3000 --name cn ceylon_news 
-
+docker pull ipmanlk/ceylonnews
+docker stop ceylon_news
+docker rm ceylon_news
 docker run -d \
   -it \
   -p 3000:3000 \
-  --name cn \
+  --name ceylon_news \
+  --restart always \
   --mount type=bind,source="$(pwd)"/auth,target=/usr/src/app/src/api/auth \
-  ceylon_news
+  ipmanlk/ceylonnews
