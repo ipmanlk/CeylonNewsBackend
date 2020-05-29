cd963e5887f17d33b1391084dd68556d15340f92
ipmanlk <io@navinda.xyz>
2020-05-29 12:13:36 +0530

    Revert "add: sematext metrics to project"
    This reverts commit 27907c3e4a0c607c9a4b9573fff043665b838843.

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 docker build --tag ceylon_news .
-docker stop ceylon_news
-docker rm ceylon_news
+docker stop cn
+docker rm cn
 # docker run -d -p 3000:3000 --name cn ceylon_news 
 
 docker run -d \
   -it \
   -p 3000:3000 \
-  --name ceylon_news \
-  --mount type=bind,source="$(pwd)"/.env,target=/usr/src/app/.env \
+  --name cn \
   --mount type=bind,source="$(pwd)"/auth,target=/usr/src/app/src/api/auth \
   ceylon_news
