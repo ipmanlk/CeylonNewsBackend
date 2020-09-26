9691bcc6c256694207785a3db96bdc8deae928bb
ipmanlk <io@navinda.xyz>
2020-09-26 23:13:52 +0530

    add: express cors middleware

--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 app.get("/:version", (req, res) => {
     try {
         const api = require(`./api/${req.params.version}/handle.js`);
         api.handle(req, res);
     } catch (error) {
+        console.log(error);
         res.status(404).json({ "error": "Sorry!. I can't find that." });
     }
 });
